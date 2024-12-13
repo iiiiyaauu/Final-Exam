1f6edac5c50321d189e1cf87115c3bf7439fbe92
iiiiyaauu <huliozyrishblo@gmail.com>
2024-12-14 00:36:24 +0600

    keyboard support

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-const display = document.getElementById('display')
+const display = document.getElementById('display');
 
 function addToDisplay(input) {
-  display.value += input
+  display.value += input;
 }
 
 function clearDisplay() {
-  display.value = ''
+  display.value = '';
 }
 
 function calculate() {
   try {
-    display.value = eval(display.value)
+    display.value = eval(display.value);
   } catch (error) {
-    display.value = 'Error'
+    display.value = 'Error';
  }
 }
+
+document.addEventListener('keydown', function(e) {
+  const keyName = e.key;
+  if (keyName === "Backspace") {
+      display.value = display.value.slice(0, -1);
+  }
+})
+
+const charactersToIgnore = 'qwertyuiop[]asdfghjkl;zxcvbnm,!@#$%^&_';
+
+document.addEventListener('keypress', function(e) {
+  const keyName = e.key;
+  
+  if (!charactersToIgnore.includes(keyName)) {
+    
+    if(keyName === '=') {
+      calculate();
+      return;
+    }
+
+    display.value += keyName;
+  }
+})
